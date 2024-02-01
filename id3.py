9f8743ef0f0ce75edf96c0e52a5613d4bb6715ca
alexpet2002 <a.petukhova2002@gmail.com>
2024-02-01 17:08:26 +0200

    modified id3.py, added split_matrix, added stopping conditions eg is_data_homogeneous, percentage_check, is_leaf_node

--- id3.py
+++ id3.py
@@ -24,7 +24,6 @@ def calculate_probabilities(matrix):
         vocab_length = len(matrix[0][0])
     except Exception as e:
         return 0
-    print(vocab_length)
     probability_matrix = [0] * vocab_length  # Initialize with a list of zeros
     matrix_size = len(matrix)
     try:
@@ -41,11 +40,28 @@ def calculate_probabilities(matrix):
 
 
 def find_index_from_word(word, vocabulary):
-    return vocabulary.index(word)
+    try:
+        return vocabulary.index(word)
+    except Exception as e:
+        print("word was null")
 
 
 def find_word_from_index(index, vocabulary):
     return vocabulary[index]
+
+
+def split_matrix(node, best_attribute):
+    data = node.matrix
+    best_attribute_index = find_index_from_word(best_attribute, node.vocabulary)
+    present = []
+    absent = []
+    # Split the array based on "yes" and "no" values
+    for i in range(len(data)):
+        if data[i][0][best_attribute_index] == 1:
+            present.append(data[i])
+        else:
+            absent.append(data[i])
+    return present, absent
 
 
 class Node:
@@ -56,7 +72,7 @@ class Node:
     # for each node the entropy and information gain should be stored
 
     def __init__(self, entropy=None, inf_gain=None, matrix=None, left_side=None, right_side=None, parent_node=None,
-                 vocabulary=None):
+                 vocabulary=None, best_attribute=None):
         self.vocabulary = vocabulary
         self.entropy = entropy  # entropy at this node
         self.inf_gain = inf_gain  # information gain of this node
@@ -64,16 +80,31 @@ class Node:
         self.left_side = left_side  # left node
         self.right_side = right_side  # right node
         self.parent_node = parent_node  # parent node
+        self.best_attribute = best_attribute  # best attribute for each node
 
     def filter_data(self, word_presence, word_index):
         filtered_data = [element for element in self.matrix if element[0][word_index] == word_presence]
         return filtered_data
 
-    def leaf_node(self):
-        all(entry[1] == 'neg' for entry in self.matrix)
+    def is_data_homogeneous(self):
+        return all(entry[1] == 'neg' for entry in self.matrix)
+
+    def percentage_check(self):
+        pass
+
+    def is_leaf_node(self):
+        return self.is_data_homogeneous() or len(self.matrix) == 1 or self.percentage_check()
+
+    def print_node(self, indent=""):
+        print(indent + "Node:")
+        print(indent + f"  Entropy per attribute: {self.entropy}")
+        print(indent + f"  Information Gain per attribute: {self.inf_gain}")
+        print(indent + f"  Matrix: {self.matrix}")
 
     def calculate_information_gain(self):
         # entropy for each attribute in the matrix
+        if len(self.matrix) == 0:
+            return 0
         probabilities = calculate_probabilities(self.matrix)
         entropies = calculate_entropy(probabilities)
         total_entropy = calculate_total_entropy(entropies)
@@ -94,30 +125,38 @@ class Node:
 
         return information_gain, total_entropy
 
+    def is_empty(self):
+        return len(self.matrix) == 0
+
     def select_word(self):
         vocabulary = self.matrix[0][0]
         best_attribute = None
         max_information_gain = -1
+        info_gain_per_attribute = []
         for i in range(len(vocabulary)):
             # for each attribute in the vocabulary, try and split the data into subsets and calculate the information
             # gain for each subset, return the best attribute
             self.split_data(1, i, self.entropy, self.inf_gain)
             # calculate information gain for each value of attribute (0 and 1)
-            # TODO: code looks questionable, review it
-            information_gain_1 = self.left_side.calculate_information_gain()[0]
-            information_gain_0 = self.right_side.calculate_information_gain()[0]
+            information_gain_1 = 0 if self.left_side.is_empty() else self.left_side.calculate_information_gain()[0]
+            information_gain_0 = 0 if self.right_side.is_empty() else self.right_side.calculate_information_gain()[0]
             # sum of information gains
             sum_of_gains = information_gain_0 + information_gain_1
+            info_gain_per_attribute.append(sum_of_gains)
+
             if sum_of_gains > max_information_gain:
                 max_information_gain = sum_of_gains
                 best_attribute = find_word_from_index(i, self.vocabulary)
+        print(best_attribute)
+        self.best_attribute = best_attribute
+        self.inf_gain = info_gain_per_attribute
         return best_attribute, max_information_gain
 
     def split_data(self, word_presence, word_index, entropy, inf_gain):
         left_side = self.filter_data(word_presence, word_index)
         right_side = self.filter_data(inverse_word_presence(word_presence), word_index)
-        left_node = Node(entropy, inf_gain, left_side, None, None, self)
-        right_node = Node(entropy, inf_gain, right_side, None, None, self)
+        left_node = Node(entropy, inf_gain, left_side, None, None, self, self.vocabulary)
+        right_node = Node(entropy, inf_gain, right_side, None, None, self, self.vocabulary)
         self.left_side = left_node
         self.right_side = right_node
         return left_node, right_node
@@ -130,23 +169,43 @@ class Tree:
         self.leaf_nodes = []
 
     def construct_tree(self, node):
-        if node.leaf_node():
+        if node.is_data_homogeneous():
             self.leaf_nodes.append(node)
-
+            return self.leaf_nodes
+        # find the best attribute based on info_gain
         best_attribute = node.select_word()[0]
+        # find the information gain for that attribute
         info_gain_of_attribute = node.select_word()[1]
-        node.split_data(1, best_attribute, node.entropy, info_gain_of_attribute)
+        # split the given data
+        node.split_data(1, find_index_from_word(best_attribute, node.vocabulary), node.entropy, info_gain_of_attribute)
+        # form 2 matrices from the initial matrix
+
         left_child_entropy = calculate_entropy(calculate_probabilities(node.left_side.matrix))
         right_child_entropy = calculate_entropy(calculate_probabilities(node.right_side.matrix))
         left_child = node.left_side
         right_child = node.right_side
+        split_result = split_matrix(node, best_attribute)
+        left_child.matrix, right_child.matrix = split_result[0], split_result[1]
         left_child.entropy = left_child_entropy
         right_child.entropy = right_child_entropy
+        node.print_node()
+        print("left node: ")
+        left_child.print_node()
+        print("right node: ")
+        right_child.print_node()
 
         self.construct_tree(left_child)
         self.construct_tree(right_child)
 
         return self.leaf_nodes
+
+    def print_tree(self, node):
+        if node is None:
+            node = self.root
+        while not (node.is_leaf_node()):
+            node.print_node()
+            print(node.left_side)
+            print(node.right_side)
 
 
 if __name__ == '__main__':
@@ -154,15 +213,11 @@ if __name__ == '__main__':
     testb = [1, 0, 0, 0]
     testac = [1, 0, 0, 1]
     test_vocabulary = ["hun", "love", "boo", "bae"]
-    test_matrix = []
-    test_matrix.append([testa, "neg"])
-    test_matrix.append([testb, "pos"])
-    test_matrix.append([testac, "neg"])
+    test_matrix = [[testa, "neg"], [testb, "pos"], [testac, "neg"]]
     print(test_matrix)
     test_entropy = calculate_entropy(calculate_probabilities(test_matrix))
     print(test_entropy)
     initial_node = Node(test_entropy, 0, test_matrix, None, None, None, test_vocabulary)
-    print(initial_node)
+    initial_node.print_node()
     test_tree = Tree(initial_node)
     test_tree.construct_tree(initial_node)
-    print(test_tree.leaf_nodes)
